bcfa93f1bef9847f16d74d3feb8013362f1e5fc7
VladimirRepp <57412538+VladimirRepp@users.noreply.github.com>
2025-03-01 22:51:46 +0700

    Add files via upload

---
+++ MyORM/MyORM/Controllers/DbUserController.cs
@@ -0,0 +1,12 @@
+﻿using MyORM.Models;
+
+namespace MyORM.Controllers
+{
+    internal class DbUserController : DbBaseController<DbUser>
+    {
+        public DbUserController(string provider_name) : base(provider_name) 
+        {
+            _tableName = "Users";
+        }
+    }
+}
---
+++ MyORM/MyORM/Global/AppSettings.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Common;
+
+namespace MyORM.Global
+{
+    public static class AppSettings
+    {
+        public static string DB_PROVIDER_NAME = "Microsoft.Data.SqlClient";
+
+        /// <summary>
+        /// Установка начальных настроек приложения: 
+        /// - регистрация фабрик провайдеров; 
+        /// - добавить при необходимости ...
+        /// </summary>
+        public static void Startup()
+        {
+            DbProviderFactories.RegisterFactory("System.Data.OleDb", System.Data.OleDb.OleDbFactory.Instance);
+            DbProviderFactories.RegisterFactory("Microsoft.Data.SqlClient", Microsoft.Data.SqlClient.SqlClientFactory.Instance);
+        }
+    }
+}
