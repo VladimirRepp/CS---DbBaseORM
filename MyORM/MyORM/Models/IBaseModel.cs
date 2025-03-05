5466831f591e1ff4c464a88c2139338a29bb05bf
VladimirRepp <57412538+VladimirRepp@users.noreply.github.com>
2025-03-05 17:29:28 +0700

    Add files via upload

---
+++ MyORM/MyORM/Models/IBaseModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Common;
+
+namespace MyORM.Models
+{
+    internal interface IBaseModel
+    {
+        int Id { get; set; }
+
+        string ToParamsInsertQueryWithID { get; }
+        string ToParamsInsertQueryWithoutID { get; }
+        string ToParamsUpdateQuery { get; }
+
+        string[] ToArrayStr { get; }
+
+        void SetData(ref DbDataReader reader);
+    }
+}
