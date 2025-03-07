92e70f5099983798642d94c7bcc8bc61636eacdd
VladimirRepp <57412538+VladimirRepp@users.noreply.github.com>
2025-03-07 16:52:50 +0700

    Update SQLQuery.sql

--- MyORM/Database/SQLQuery.sql
+++ MyORM/Database/SQLQuery.sql
@@ -7,13 +7,11 @@ CREATE TABLE Users (
     TelegramId VARCHAR(100) NOT NULL UNIQUE,
     FullName VARCHAR(255) NOT NULL,
     NickName VARCHAR(255),
-    Login VARCHAR(100) NOT NULL UNIQUE,
     Password VARCHAR(255) NOT NULL,
-    Role VARCHAR(50) NOT NULL
+    Role VARCHAR(50) NOT NULL -- Values: Student, Teacher, Admin
 );
-
-Truncate TABLE Users;
 
 Select * from Users;
 
-Drop table Users;
+--Truncate table Users;
+--Drop table Users;
